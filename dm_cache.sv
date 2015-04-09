3a5165c5ce79a2a2e533fbef1d4d83f63e9b129b
lamemmett <lamemmet@EE-361PC05.ee.washington.edu>
2015-04-09 13:46:05 -0700

    DM_cache update

--- dm_cache.sv
+++ dm_cache.sv
@@ -1,105 +1,111 @@
 `protect // associativity 
-module dm_cache (data_out, req_out, miss_out, addr_in, data_in, req_in, miss_in, reset, clk);
+module dm_cache (data_out, found_data, miss, addr_in, data_in, writeEnable, enable, reset, clk);
 	parameter SIZE = 128;
 	parameter ADDR_LENGTH = 10;
 	parameter DELAY = 50;
-	parameter BLOCK_SIZE = 4;
+	parameter BLOCK_SIZE = 32;
 	
 	parameter COUNTER_SIZE = $clog2(DELAY);
 	
-	parameter BYTE_SELECT_SIZE = $clog2(BLOCK_SIZE); // 2
-	parameter INDEX_SIZE = $clog2(SIZE/BLOCK_SIZE/8); // 2
+	parameter BYTE_SELECT_SIZE = $clog2(BLOCK_SIZE/8); // 2
+	parameter INDEX_SIZE = $clog2(SIZE/BLOCK_SIZE); // 2
 	parameter TAG_SIZE = ADDR_LENGTH - BYTE_SELECT_SIZE - INDEX_SIZE; // 6
 	
-	output reg [7:0] data_out; // 8 bits
-	output reg req_out, miss_out;
+	output reg [31:0] data_out; // 8 bits
+	output reg found_data, miss = 0;
 	
 	input [(ADDR_LENGTH-1):0] addr_in; // 10 bits
 	input [(BLOCK_SIZE-1):0] data_in;
-	input req_in, miss_in, reset, clk;
-
-	reg [(INDEX_SIZE-1):0] [(BLOCK_SIZE-1):0] mem;
-	reg [(INDEX_SIZE-1):0] [(TAG_SIZE-1):0] tag;
-	reg [(INDEX_SIZE-1):0] valid;
+	input writeEnable, enable, reset, clk;
 	
-	reg waiting;
+	wire [BYTE_SELECT_SIZE-1:0] 	byteSelect 	= addr_in[(BYTE_SELECT_SIZE-1):0];
+	wire [INDEX_SIZE-1:0]			cacheIndex 	= addr_in[(BYTE_SELECT_SIZE+INDEX_SIZE-1):(BYTE_SELECT_SIZE)];
+	wire [TAG_SIZE-1:0]				tag 			= addr_in[(ADDR_LENGTH-1):(BYTE_SELECT_SIZE+INDEX_SIZE)];
+
+	reg [(INDEX_SIZE-1):0] [(BLOCK_SIZE-1):0] data;
+	reg [(INDEX_SIZE-1):0] [(TAG_SIZE-1):0] tags;
+	reg [(INDEX_SIZE-1):0] valid_bits;
+	
+	initial begin
+		valid_bits[0] = 1;
+		tags[0] = 6'b000011;
+		data[0] = 32'hFFFFFFFF;
+	end
+	
+	reg waiting = 0;
 	reg [COUNTER_SIZE-1:0] counter;
-	reg finish_delay;
-	reg found_data;
+	reg finish_delay = 0;
 	reg [31:0] data_f;
 	
-	integer i, j;
-	// increment counter per clock cycle until delay is reached
-	always @(posedge clk) begin
-		if(reset) begin
-			for(i = 0; i < INDEX_SIZE; i++) begin
-					valid[i] = 1'b0;
-					tag[i] = 6'b000000;
-					mem[i] = 32'h00000000;
-			end
-		end
-		if(waiting) begin
-			if(req_in = 1'b1) begin
-				data_out = data_in;
-				waiting = 1'b0;
-			end
-		end
-		else begin
-			if(miss_in) begin
-				counter++;
-				if (counter == (DELAY-1)) begin
-					finish_delay = 1'b1;
-					counter = 1'b0; 
-				end
-			end
-			else begin
-				counter = 0; // initialize counter
-				req_out = 1'b0;
-				miss_out = 1'b0;
-				finish_delay = 1'b0;
-				found_data = 1'b0;
-				data_out = 32'hXXXXXXXX;
-			end
-			if(finish_delay) begin
-				for(j = 0; j < INDEX_SIZE; j++) begin
-					if(tag[j] == addr_in[(ADDR_LENGTH-1):(BYTE_SELECT_SIZE+INDEX_SIZE)] && valid[j] == 1'b1) begin
-						found_data = 1'b1;
-						data_out = mem[j];
-					end
-				end
-			end
-			if(!found_data) begin
-				waiting = 1'b1;
-			end
-		end
+	// enable signal initializes counter nulls the output value
+	always @(posedge enable) begin
+		miss = 0;
+		counter = 1;
+		waiting = 1;
+		finish_delay = 0;
+		data_out = 8'bx;
+		found_data = 1'bx;
 	end
+	
+	// increment counter per clok cycle, once delay has been reached set flags
+	always @(posedge clk) begin
+		if (waiting) begin
+			counter++; end
+		if (counter == DELAY) begin
+			waiting = 0;
+			finish_delay = 1; end
+	end
+	
+	// Once delay done, check if data exists and return it
+	always @(posedge finish_delay) begin
+		if (tags[cacheIndex] == tag && valid_bits[cacheIndex] == 1) begin
+			found_data = 1;
+			data_out = data[cacheIndex][31:0]; end
+		else begin
+			miss = 1;
+			found_data = 0; end
+	end
+	
+	// wait for lower cache to return data
+	always @(posedge writeEnable) begin
+		
+		found_data = 1;
+	end	
 endmodule
 `endprotect
 
 module dm_cache_testbench();
-	wire [31:0] data_out; // always 32 bits
-	wire req_out, miss_out;
+	wire [31:0] data_out, dontCare; // always 1 byte
+	wire found_data, miss;
 	
-	reg [8:0] addr_in;
+	reg [9:0] addr_in;
 	reg [31:0] data_in;
-	reg req_in, miss_in, reset, clk;
+	reg writeEnable, enable, reset, clk;
 	
 	parameter t = 10;
 	parameter d = 20;
 	
-	dm_cache	test (.data_out, .req_out, .miss_out, .addr_in, .data_in, .req_in, .miss_in, .reset, .clk);
+	dm_cache	cache (.data_out, .found_data, .miss, .addr_in, .data_in, .writeEnable, .enable, .reset, .clk);
+	//mainMem	memory (.data_out(dontCare), requestComplete, data_in, addr, we, enable, clk)
 	
 	always #(t/2) clk = ~clk;
 	
 	initial begin
+		clk = 0;
 		reset = 1'b1; #(t/2);
 		reset = 1'b0; #(t/2);
 		
 		#(d*t);
 		
 		addr_in = 50;
+		enable = 1;
+		#(100*t);
 		
-		#(d*5*t);
+		addr_in = 0;
+		enable = 0;
+		#t;
+		enable = 1;
+		#(100*t);
 		$stop;
 	end
 endmodule 
